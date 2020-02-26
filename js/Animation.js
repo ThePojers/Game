4d5d0f10b751de48f1fbf37f9d98873f225f4c54
Dawid Perzyński <loginosobisty@o2.pl>
2020-02-26 18:37:40 +0100

    Add working Left hand

--- js/Animation.js
+++ js/Animation.js
@@ -1,15 +1,49 @@
 
-function animationPlayer(playerInput) {
+function animationPlayer(playerInput, reset) {
     if( playerInput == 1) {
+        document.getElementById("kamieńfinish").style.display = "none";
         document.getElementById("kamień").style.display = "block";
-        document.getElementById("nomove").style.display = "none";
+        document.getElementById("start").style.display = "none";
         setTimeout(
             () => {
               console.log('Hello after 4 seconds');
               document.getElementById("kamień").style.display = "none";
-              document.getElementById("nomove").style.display = "block";
+              document.getElementById("kamieńfinish").style.display = "block";
             },
             1 * 1000
           );
+    }
+    else if( playerInput == 2) {
+        document.getElementById("papierfinish").style.display = "none";
+        document.getElementById("papier").style.display = "block";
+        document.getElementById("start").style.display = "none";
+        setTimeout(
+            () => {
+              console.log('Hello after 4 seconds');
+              document.getElementById("papier").style.display = "none";
+              document.getElementById("papierfinish").style.display = "block";
+            },
+            1 * 1000
+          );
+    }
+    else if( playerInput == 3) {
+        document.getElementById("nożycefinish").style.display = "none";
+        document.getElementById("nożyce").style.display = "block";
+        document.getElementById("start").style.display = "none";
+        setTimeout(
+            () => {
+              console.log('Hello after 4 seconds');
+              document.getElementById("nożyce").style.display = "none";
+              document.getElementById("nożycefinish").style.display = "block";
+            },
+            1 * 1000
+          );
+    }
+    else if(playerInput == 4) {
+        clearMessages()
+        document.getElementById("nożycefinish").style.display = "none";
+        document.getElementById("papierfinish").style.display = "none";
+        document.getElementById("kamieńfinish").style.display = "none";
+        document.getElementById("start").style.display = "block";
     }
 }
--- js/script.js
+++ js/script.js
@@ -23,6 +23,11 @@ function playGame (playerInput){
     displayResults(argComputerMove, argPlayerMove);
 }
 
+document.getElementById("ressetto").addEventListener('click', function (){ 
+    playerInput = 4
+    animationPlayer (playerInput)
+});
+
 document.getElementById("Play-Kamień").addEventListener('click', function (){ 
     playerInput = 1,
     playGame (playerInput)
@@ -32,11 +37,13 @@ document.getElementById("Play-Kamień").addEventListener('click', function (){
 document.getElementById("Play-Papier").addEventListener('click', function (){ 
     playerInput = 2,
     playGame (playerInput)
+    animationPlayer (playerInput)
 });
 
 document.getElementById("Play-Nożyce").addEventListener('click', function (){ 
     playerInput = 3,
     playGame (playerInput)
+    animationPlayer (playerInput)
 });
 
 
