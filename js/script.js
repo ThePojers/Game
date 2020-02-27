f35131631588023534e35c48297a0457d13c2573
Dawid Perzyński <loginosobisty@o2.pl>
2020-02-27 12:54:27 +0100

    Fixed bug with randomNumber

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-function playGame (playerInput){
-    // ruch komputera 
-    clearMessages()
-    
+function playGame (playerInput, randomNumber){
+        // ruch komputera 
+
+        clearMessages()
 
         let argComputerMove = getMoveName(randomNumber);
 
@@ -20,15 +20,14 @@ function playGame (playerInput){
 
         displayResults(argComputerMove, argPlayerMove);
 
-    //     },
-    //     4 * 1000
-    // );
 }
 
 // document.getElementById("ressetto").addEventListener('click', function (){ 
 //     playerInput = 4
 //     animationPlayer (playerInput)
 // });
+
+// Music
 var BGM = document.getElementById("mussik");
 BGM.autoplay = true;
 BGM.volume = 0.2;
@@ -40,36 +39,41 @@ var youwin  = document.getElementById("YouWin");
 
 var draw = document.getElementById("draw");
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-    
-console.log('Wylosowana liczba to: ' + randomNumber);
+
+
 
 document.getElementById("Play-Kamień").addEventListener('click', function (){ 
-    console.log ("0");
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    
+    console.log('Wylosowana liczba to: ' + randomNumber);
     playerInput = 1,
     console.log ("1");
     compMoveAnimation(randomNumber)
-    console.log ("2");
+    console.log ("random w animacji" + randomNumber);
     animationPlayer (playerInput)
     console.log ("3");
-    playGame (playerInput)
+    playGame (playerInput, randomNumber)
     console.log ("4");
 });
 
-document.getElementById("Play-Papier").addEventListener('click', function (){ 
+document.getElementById("Play-Papier").addEventListener('click', function (){
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    
+    console.log('Wylosowana liczba to: ' + randomNumber);
     playerInput = 2,
-    playGame (playerInput)
+    playGame (playerInput, randomNumber)
     compMoveAnimation(randomNumber)
     animationPlayer (playerInput)
 });
 
 document.getElementById("Play-Nożyce").addEventListener('click', function (){ 
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    
+    console.log('Wylosowana liczba to: ' + randomNumber);
     playerInput = 3,
-    playGame (playerInput)
+    playGame (playerInput, randomNumber)
     compMoveAnimation(randomNumber)
     animationPlayer (playerInput)
 });
-
-
 
 
